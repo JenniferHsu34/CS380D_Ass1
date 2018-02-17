f05808e21b0d2967c14f8d46a6bda8f586b98bb0
cchsu <cchsu@utexas.edu>
2018-02-17 16:29:49 -0600

    new testing

--- testing.py
+++ testing.py
@@ -1,70 +1,123 @@
 import unittest
 import sys
-import master
-print 'here?#################################'
-'''
+from master import *
+testCase =5
 
-class TestKV(unittest.TestCase):
-
+debugV = 0
 
 
-    def test_1(self): # test get with invalid key
-        joinServer(0)
-        joinServer(1)
+def debug(s):
+    global debugV
+    print(str(s)  + " debug information " + str(debugV))
+    debugV = debugV + 1
 
-        joinClient(0, 0)
-        joinClient(1, 1)
-        self.assertEqual(get(0, "x"), 'ERR_KEY', 'invalid key')
-    def test_2(self): # test get with valid key
-        put(0, "x", 0)
-        put(1, "x", 1)
 
-        self.assertEqual(get(0, "x"), 0)
-        self.assertEqual(get(1, "x"), 1)
+if testCase == 0:
+    joinServer(0)
+    time.sleep(0.05)
+    #time.sleep(1)
+    joinClient(0, 0)
 
-    def test_3(self): # put more value and check wlog
-        put(0, "x", 3)
-        put(0, "y", 3)
-        curLog0 =  [sys.maxsize, 1, 0, ('x', 0)], \
-                         [sys.maxsize, 2, 0, ('x', 3)], [sys.maxsize, 3, 0, ('y', 3)]
-        self.assertEqual(curLog0, servers[0].writeLog)
-        curLog1 =  [sys.maxsize, 1, 1, ('x', 1)]
-        self.assertEqual(curLog1, servers[1].writeLog)
+    put(0,"x", 0)
+    breakConnection(0, 0) # c, s
+    joinServer(1)
+    time.sleep(0.05)
+    createConnection(0, 1)
+    put(0,"x", 1) # now, data should only send to server1
+    get(0, "x") # 1
+    breakConnection(0, 1)
+    createConnection(0, 0) # now, c0 only connect to s0
+    get(0, "x") # should output ERR_DEP
+    createConnection(0, 1)
+    stabilize()
+    get(0, "x") # should get from s0? output 1
+#################################################
+elif testCase == 1:
+    # 1 s, 2 c
+    joinServer(0)
+    joinClient(0, 0)
+    joinClient(1, 1)
+    put(0,"x", 0)
+    put(1,"x", 1)
+    put(0,"y", 2)
+    put(1,"y", 3)
+    get(0, "x")  # 0
+    get(1, "x")  # 1
+    stabilize()
+    get(0, "x")  # 1
+    get(1, "x")  # 1
+    ## critical
+    put(0,"x", 4)
+    get(0, "x")  # 4
+    breakConnection(0, 0)
+    get(0, "x")  # ERR_DEP
 
-    def test_4(self): # stablize
-        stabilize()
-        
+##################################################
+elif testCase == 2:
+    # 1 s, 2 c
 
-if __name__ == '__main__':
-    unittest.main()
-#
-'''
-print 'here?#################################'
-joinServer(0)
-print 'here?#################################'
-joinServer(1)
-print 'here?#################################'
+    joinServer(0)
+    joinClient(0, 0)
+    joinClient(1, 1)
+    put(0,"x", 0)
+    get(1, "x")  # 0
+    put(1,"x", 1)
+    put(0,"x", 2)
+    put(0,"x", 3)
+    get(1, "x")  # 1 !!! really important
+    stabilize()
+    get(1, "x")  # 3
+    put(0,"x", 4)
 
-joinClient(0, 0)
-print 'here?#################################'
-joinClient(1, 1)
-print 'here?################'
-connectServers(0,1)
-print 'here?#'
-print get(0, "z")
-put(0, "x", 0)
-put(1, "x", 1)
-put(0,"x", 2)
-put(0,"y", 3)
-print '-----before stablize'
-print '[s0]', servers[0].vclock, servers[0].writeLog
-print '[s1]', servers[1].vclock, servers[1].writeLog
-print 'get x from s0: (exp 2) ', get(0, "x") # should get 2
-print 'get x from s1: (exp 1)', get(1, "x") # should get 1
-stabilize()
-print '-----after stablize'
-print '[s0]', servers[0].vclock, servers[0].writeLog #vclock -> 3, 1, 0, 0
-print '[s1]', servers[1].vclock, servers[1].writeLog #vclock -> 3, 1, 0, 0
-print 'get x from s1: (exp 2 since it did anti entropy)', get(1, "x") # should get 1
-#joinClient(1,0)
+###################################################
+elif testCase == 3:
+    #breakservers
+    joinServer(0)
+    joinServer(1)
+    joinServer(2)
+    joinServer(3)
+    joinServer(4)
+    joinClient(0, 0)
+    put(0,"x", 0)
+    createConnection(0, 1) # c0 with s1
+    breakConnection(0, 0)
+
+    put(0,"x", 2) # put x:2 in server 1
+    breakConnection(0, 1) # break server 1
+    get(0, "x") #  ERR_DEP
+    breakservers(0, 1)
+    stabilize() # do nothing
+    get(0, "x") #  ERR_DEP
+    connectServers(0, 1)
+    stabilize()
+    get(0, "x") # 2
+else:
+    # TESTING
+    debug("ffff")
+
+    debug("fffwwwww")
+    joinServer(0)
+    joinServer(1)
+    time.sleep(1)
+    joinClient(0, 0)
+    joinClient(1, 1)
+
+    connectServers(0, 1)
+
+    get(0, "z")
+    put(0, "x", 0)
+    # get(0, "x")
+
+    put(1, "x", 1)
+    put(0, "x", 2)
+    put(0, "y", 3)
+
+    print ('-----before stablize')
+    debug(get(0, "x"))  # should get 2
+    print ('get x from s1: (exp 1)')
+    get(1, "x")  # should get 1
+    stabilize()
+    # time.sleep(1)
+    print ('-----after stablize')
+    get(1, "x")  # should get 1
 
