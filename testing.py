14a9ec4db177030efa594be4b46d1b1f4563974a
cchsu <cchsu@utexas.edu>
2018-02-17 18:17:57 -0600

    Merge branch 'master' of /u/cchsu/PycharmProjects/CS380D_Ass1 with conflicts.

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 from master import *
-testCase =1
+testCase =0
 
 debugV = 0
 '''
@@ -13,58 +13,55 @@ def debug(s):
     print(str(s)  + " debug information " + str(debugV))
     debugV = debugV + 1
 
+#set up servers with corresponding clients and make them fully connected
+def setup(numServers):
+    for i in range(numServers):
+        joinServer(i)
+        time.sleep(0.05)
+        joinClient(i, i)
+
+    for i in range(numServers):
+        for j in range(numServers):
+            if(i<j):
+                connectServers(i,j)
+
+
+
+'''
 
 if testCase == 0:
-    joinServer(0)
-    time.sleep(0.05)
-    #time.sleep(1)
-    joinClient(0, 0)
-    print ("---[TEST 0] should output 1 ERR_DEP 1---")
-    put(0,"x", 0)
-    #breakConnection(0, 0) # c, s
-    joinServer(1)
-    time.sleep(0.05)
-    createConnection(0, 1)
-    put(0,"x", 1) # now, data should only send to server1
-    get(0, "x") # 1
-    #breakConnection(0, 1)
-    createConnection(0, 0) # now, c0 only connect to s0
-    get(0, "x") # should output ERR_DEP
-    createConnection(0, 1)
+    setup(2)
+    for i in range(100):
+        put(0, str(i), i)
+        put(1, "y", -i)
     stabilize()
-    get(0, "x") # should get from s0? output 1
+    for i in range(100):
+        put(0, str(i), 100+i)
+        put(1, str(i),100-i )
+    get(0, "x")  # 0
+    get(1, "x")  # 1
+    print("finished")
+
 #################################################
 elif testCase == 1:
     print ("---[TEST 1] should output 0 1 1 1 4 ERR_DEP---")
-    # 1 s, 2 c
-    joinServer(0)
-    joinServer(1)
-    connectServers(0,1)
-    time.sleep(0.05)
-    joinClient(0, 0)
-    joinClient(1, 0)
-    time.sleep(0.05)
+    setup(2)
     put(0,"x", 0)
     put(1,"x", 1)
     put(0,"y", 2)
-    time.sleep(0.05)
     put(1,"y", 3)
     get(0, "x")  # 0
     get(1, "x")  # 1
-    printStore(0)
     stabilize()
-    time.sleep(0.1)
-    printStore(0)
-    printStore(1)
     get(0, "x")  # 1
     get(1, "x")  # 1
     ## critical
     put(0,"x", 4)
+
     time.sleep(0.05)
     get(0, "x")  # 4
     time.sleep(0.05)
     get(0, "x")  # ERR_DEP
-    printStore(1)
     print("finished")
 
 ##################################################
@@ -72,8 +69,6 @@ elif testCase == 2:
     # 1 s, 2 c
     print ("---[TEST 2] should output 0 1 3 4---")
     joinServer(0)
-    joinServer(1)
-    connectServers(0, 1)
     time.sleep(0.05)
     joinClient(0, 0)
     joinClient(1, 0)
@@ -88,7 +83,6 @@ elif testCase == 2:
     get(1, "x")  # 3
     put(0,"x", 4)
     get(0, "x") # 4
-    print("finished")
 ###################################################
 elif testCase == 3:
     print ("---[TEST 3] should output ERR_DEP ERR_DEP 2---")
@@ -119,26 +113,51 @@ elif testCase == 3:
     connectServers(0, 1)
     stabilize()
     get(0, "x") # 2
+
+elif testCase == 4:
+    #test killServer
+    joinServer(0)
+    time.sleep(0.05)
+    joinServer(1)
+    time.sleep(0.05)
+    joinServer(2)
+    time.sleep(0.05)
+    joinServer(3)
+    time.sleep(0.05)
+    joinServer(4)
+    time.sleep(0.05)
+    joinClient(0, 3)
+    time.sleep(0.05)
+    put(0, "x", 0)
+    put(0, "x", 1)
+    get(0, "x") # 1
+    createConnection(0, 4)
+    put(0, "x", 2)
+    get(0, "x") # 4
+    killServer(4)
+    get(0, "x") # 4 or 1?
+    createConnection(0, 3)
+
+elif testCase == 5:
+    numServer = 5
+    setup(numServer)
+    keys = list(string.ascii_lowercase)
+    gt = [[None] * len(keys) for range(numServer)] # ground truth
+    for i in xrange(10):
+        for j in range(numServer):
+            kId, v = genPair()
+            gt[j][kId] = v
+            put(j, keys[kId], v)
+
+    for i in range(len(keys)):
+        for j in range(numServer):
+            real = gt[j][i] if gt[j][i]!= None else 'ERR_KEY'
+
+
 else:
     # TESTING
-    debug("ffff")
+    setup(3)
 
-    debug("fffwwwww")
-    joinServer(0)
-    joinServer(1)
-    time.sleep(1)
-    joinClient(0, 0)
-    joinClient(1, 1)
-
-    connectServers(0, 1)
-
-    get(0, "z")
-    put(0, "x", 0)
-    # get(0, "x")
-
-    put(1, "x", 1)
-    put(0, "x", 2)
-    put(0, "y", 3)
 
     print ('-----before stablize')
     debug(get(0, "x"))  # should get 2
@@ -148,4 +167,43 @@ else:
     # time.sleep(1)
     print ('-----after stablize')
     get(1, "x")  # should get 1
+'''
 
+import unittest
+import string
+def genPair(krange, vrange):
+    kid= randint(0, krange-1)
+    val = randint(0, vrange-1)
+    return kid, val
+
+
+class TestKV(unittest.TestCase):
+    """
+    Test the add function from the mymath library
+    """
+
+    def test1(self):
+        numServer = 2
+        setup(numServer)
+        #keys = list(string.ascii_lowercase)
+        keys = ['a', 'b', 'c']
+        gt = [[None] * len(keys) for i in range (numServer)]  # ground truth
+        for i in xrange(10):
+            for j in range(numServer):
+                kId, v = genPair(len(keys), 10000)
+                gt[j][kId] = v
+                #print 'put ', j, keys[kId], v
+                put(j, keys[kId], v)
+                time.sleep(0.05)
+                get(j, keys[kId])
+        #print gt
+        for i in range(len(keys)):
+            for j in range(numServer):
+                real = gt[j][i] if gt[j][i] != None else 'ERR_KEY'
+                time.sleep(0.05)
+                get(j, keys[i])
+                self.assertEqual(real, get(j, keys[i]))
+
+
+if __name__ == '__main__':
+    unittest.main()
